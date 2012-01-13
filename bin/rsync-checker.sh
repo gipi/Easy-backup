8374dddbf3f80002bb98d717c333d8744b1d396a
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-01-13 13:11:39 +0100

    Add rsync functionality.

---
+++ bin/rsync-checker.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# this checks that rsync was the only command called
+# TODO: check also shell substitution
+#echo "SSH_ORIGINAL_COMMAND: $SSH_ORIGINAL_COMMAND"
+case "$SSH_ORIGINAL_COMMAND" in
+        *\&*)
+                echo "Rejected 1"
+                ;;
+        *\;*)
+                echo "Rejected 2"
+                ;;
+        # the absolute path otherwise "protocol version mismatch -- is your shell clean?"
+        /usr/bin/rsync*)
+                $SSH_ORIGINAL_COMMAND
+                ;;
+        *true*)
+                echo $SSH_ORIGINAL_COMMAND
+                ;;
+        *)
+                echo "Rejected 3"
+                ;;
+esac
